247abd10e226d179c419448f5121170f2bd924cf
lukasjoho <social@lukashoppe.com>
2021-05-02 12:06:22 +0200

    Change fontsource call to browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,7 @@
 // import Layout from "./layouts/Layout"
 
 // export const wrapPageElement = ({ element }) => <Layout>{element}</Layout>
+import "@fontsource/poppins/500.css"
+import "@fontsource/poppins/400.css"
+import "@fontsource/poppins/300.css"
+import "@fontsource/oswald/700.css"
